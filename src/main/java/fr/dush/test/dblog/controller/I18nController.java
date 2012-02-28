03be5490c1244fdb9be568be374787a4076dad5c
Thomas Duchatelle <thomas.duchatelle@capgemini.com>
2012-02-28 23:44:57 +0100

    Internationnalisation finie.

--- src/main/java/fr/dush/test/dblog/controller/I18nController.java
+++ src/main/java/fr/dush/test/dblog/controller/I18nController.java
@@ -3,15 +3,19 @@ package fr.dush.test.dblog.controller;
 import java.util.Collection;
 import java.util.Locale;
 
+import javax.faces.component.html.HtmlCommandLink;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.springframework.context.annotation.Scope;
+
 import fr.dush.test.dblog.dto.i18n.AvailableLocale;
 import fr.dush.test.dblog.services.II18nManager;
 
 @Named("i18n")
+@Scope("session")
 public class I18nController {
 
 	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(I18nController.class);
@@ -19,19 +23,46 @@ public class I18nController {
 	@Inject
 	private II18nManager i18nManager;
 
+	private Locale locale;
+
 	public AvailableLocale[] getAvailableLocalesList() {
 		final Collection<AvailableLocale> locales = i18nManager.getAvailableLocales();
+		logger.debug("getAvailableLocalesList {}", locales);
 		return locales.toArray(new AvailableLocale[locales.size()]);
 	}
 
-	public void changeLocale(final Locale locale) {
+	/**
+	 * Change la langue de la page courrante et sauvegarde la préférence.
+	 * @param locale
+	 */
+	protected void changeLocale(final Locale locale) {
 		FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+		this.locale = locale;
 	}
 
+	/**
+	 * Méthode appellable à partir d'un HtmlCommandLink pour changer la langue du site.
+	 * @param event Event de source HtmlCommandLink qui détient un attribut "locale" de type Locale.
+	 */
 	public void onChangeLocale(final ActionEvent event) {
-		logger.info(" --> onChangeLocale  ({})", event);
-		final String strLocale = event.getComponent().getId();
-		logger.warn("Change language to {}", strLocale);
-		changeLocale(new Locale(strLocale));
+		if(event.getSource() instanceof HtmlCommandLink) {
+			final Object obj = ((HtmlCommandLink)event.getSource()).getAttributes().get("locale");
+			logger.debug(" --> onChangeLocale  ({})", obj);
+			if(obj instanceof Locale) {
+				changeLocale((Locale) obj);
+			}
+		} else {
+			logger.warn("Unknown event {}", event);
+		}
+	}
+
+	/**
+	 * Renvoie la locale de la session en cours...
+	 * @return
+	 */
+	public Locale getLocale() {
+		if(locale == null) return FacesContext.getCurrentInstance().getViewRoot().getLocale();
+
+		return locale;
 	}
 }
