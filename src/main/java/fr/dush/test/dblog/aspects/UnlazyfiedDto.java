fc2c518e9134f11348b8d2e505eea55dec5a2234
Thomas Duchatelle <duchatelle.thomas@gmail.com>
2012-06-06 23:20:09 +0200

    AOP qui intercepte des méthode qui ne servent pas à grand chose...

---
+++ src/main/java/fr/dush/test/dblog/aspects/UnlazyfiedDto.java
@@ -0,0 +1,48 @@
+package fr.dush.test.dblog.aspects;
+
+import javax.inject.Named;
+
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.hibernate.Hibernate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import fr.dush.test.dblog.dto.model.Ticket;
+
+/**
+ * Délazyfie un objet DTO de ses collection hibernate.
+ *
+ * @author Thomas Duchatelle (duchatelle.thomas@gmail.com)
+ *
+ */
+@Named
+@Aspect
+public class UnlazyfiedDto {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(UnlazyfiedDto.class);
+
+	public UnlazyfiedDto() {
+		super();
+		LOGGER.warn("UnlazyfiedDto contructor");
+	}
+
+	@AfterReturning(pointcut = "execution(* fr.dush.test.dblog.dao.model.hibernate.TicketDAOImpl.*(..))", returning = "object")
+	public void delazyfiedDto(Object object) {
+		if (object instanceof Ticket) {
+			Ticket t = (Ticket) object;
+			Hibernate.initialize(t);
+			t.setMessage("Hacked message : " + t.getMessage());
+			LOGGER.info("{} is initialized !", object);
+		} else {
+			LOGGER.error("Object {} is not a Ticket...", object);
+		}
+
+	}
+
+	@AfterReturning("execution(* fr.dush.test.dblog.dao.model.hibernate.TicketDAOImpl.*(..))")
+	public void logAfter() {
+		LOGGER.warn("Hello world !");
+	}
+
+}
---
+++ src/test/java/fr/dush/test/dblog/aspects/UnlazyfiedDtoTest.java
@@ -0,0 +1,30 @@
+package fr.dush.test.dblog.aspects;
+
+import static org.junit.Assert.*;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+
+import fr.dush.test.dblog.dao.model.ITicketDAO;
+import fr.dush.test.dblog.dto.model.Ticket;
+import fr.dush.test.dblog.engine.AbstractJunitTest;
+import fr.dush.test.dblog.engine.dbunitapi.DatabaseScripts;
+
+@DatabaseScripts(locations = { "/bdd/tickets.xml" })
+@ContextConfiguration(locations = { "classpath:WEB-INF/spring/context-aop.xml" })
+public class UnlazyfiedDtoTest extends AbstractJunitTest {
+
+	@Inject
+	private ITicketDAO ticketDAO;
+
+	@Test
+	public void testAMethod() throws Exception {
+		final Ticket t = ticketDAO.findById(4);
+
+		ticketDAO.findPage(0, 12);
+		assertTrue(t.getMessage() + " not begin with Hacked.", t.getMessage().startsWith("Hacked"));
+	}
+
+}
